06e06e3a06ffc913b8ace4da5428beb73cf8cfb4
Martín Stanicio <martuytimo@gmail.com>
2026-05-25 15:03:54 +0000

    solve 102697/010

---
+++ 102697/010.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    points = int(input())
+    games = int(input())
+
+    points_per_game = points / games
+
+    print(points_per_game)
+
+
+if __name__ == "__main__":
+    main()
